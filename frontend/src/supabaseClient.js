e06a853bf1340a084d7ba85336cfa53aa61454d3
VishistS <vishistsantanam@gmail.com>
2025-08-16 11:53:55 -0500

    fixed forgot password link not being detected

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -8,7 +8,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey,
   auth: {
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: true,
   },
 }
 );
